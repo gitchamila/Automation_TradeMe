29cbd573810d5180fced6a68ee047a95c7df58f9
Chamila <chamilamadusankha@gmail.com>
2025-03-20 11:50:45 +1300

    create jobs pages

---
+++ features/TradeMeLoging.feature
@@ -0,0 +1,6 @@
+Feature: Tread me Ecommerce loging validations
+
+    Scenario: Loging to the TreadMe
+        Given Loing to TreadMe Ecommerce application with "chamilalook@gmail.com" and "Chamila@1828"
+        When Enter valid credentials
+        Then Verify user can log in to the application
---
+++ features/step_definitions/step.js
@@ -0,0 +1,22 @@
+const { When, Then, Given } = require('@cucumber/cucumber')
+const { test, expect, playwright } = require('@playwright/test')
+
+
+Given('Loing to TreadMe Ecommerce application with {un} and {pw}', async function (username, password) {
+    // Write code here that turns the phrase above into concrete actions
+    const browser = playwright.chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newpage(page);
+
+
+});
+
+When('Enter valied credintilas', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('Verify user can loging to the application', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
